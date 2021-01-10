d1491ffbd7b3602844eea55887b32b380c21740f
ChiBao <chibao2769@yahoo.com>
2021-01-10 09:25:38 +0700

    img product, list_item Main, list_fastfood, list_rice,...........................

--- BookingRestaurant/app/src/main/java/com/example/bookingrestaurant/MainActivity.java
+++ BookingRestaurant/app/src/main/java/com/example/bookingrestaurant/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.Button;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1;
+    Button btn01, btn02, btn03, btn04;
     private ActionBar toolBar;
 
 
@@ -26,12 +26,38 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation_bottom);
         navigation.setOnNavigationItemSelectedListener(mOnNavigation);
 
-        btn1 = (Button) findViewById(R.id.btn01);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btn01 = (Button)findViewById(R.id.btn_fastfood);
+        btn01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Reg_users.class);
+                Intent intent = new Intent(MainActivity.this, fastfood.class);
+                startActivity(intent);
+            }
+        });
+
+        btn02 = (Button)findViewById(R.id.btn_milktea);
+        btn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, milktea.class);
+                startActivity(intent);
+            }
+        });
+
+        btn03 = (Button)findViewById(R.id.btn_rice);
+        btn03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, rice.class);
+                startActivity(intent);
+            }
+        });
+
+        btn04 = (Button)findViewById(R.id.btn_soup);
+        btn04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, soup.class);
                 startActivity(intent);
             }
         });
