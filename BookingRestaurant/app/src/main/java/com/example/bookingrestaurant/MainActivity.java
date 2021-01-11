380d441fb3272887f10aa8b2ccecf609ba41eab5
Dũng Hoàng <dung.hoang111299@gmail.com>
2021-01-11 09:11:42 +0700

    clear import

--- BookingRestaurant/app/src/main/java/com/example/bookingrestaurant/MainActivity.java
+++ BookingRestaurant/app/src/main/java/com/example/bookingrestaurant/MainActivity.java
@@ -1,18 +1,7 @@
 package com.example.bookingrestaurant;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
